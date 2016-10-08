7125ebed852b65e08f244e56b47be81b67d032b9
Jagdeep Gill <jgill.tftf@gmail.com>
2016-10-08 01:39:08 -0700

    add import for vendor librares - @angular/{forms, platform-browser-dynamic}

--- dev/frontend/app/vendor.js
+++ dev/frontend/app/vendor.js
@@ -1,13 +1,13 @@
 "use strict";
 // Angular
-require('@angular/platform-browser');
-// import '@angular/platform-browser-dynamic';
-require('@angular/core');
-require('@angular/common');
-// import '@angular/http';
-require('@angular/router');
+require("@angular/platform-browser");
+require("@angular/platform-browser-dynamic");
+require("@angular/core");
+require("@angular/common");
+require("@angular/forms");
+require("@angular/router");
 // RxJS
-require('rxjs');
+require("rxjs");
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 //# sourceMappingURL=vendor.js.map
--- dev/frontend/app/vendor.ts
+++ dev/frontend/app/vendor.ts
@@ -1,9 +1,9 @@
 // Angular
 import '@angular/platform-browser';
-// import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-// import '@angular/http';
+import '@angular/forms';
 import '@angular/router';
 // RxJS
 import 'rxjs';
