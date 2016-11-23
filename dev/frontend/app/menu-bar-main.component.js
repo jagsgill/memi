7acca24f240638401ff0e4c6cc081a0797d17816
Jagdeep Gill <jgill.tftf@gmail.com>
2016-11-22 16:03:22 -0800

    Fix navigation menu to show active tab: added click event to set styles, fixed styles

--- dev/frontend/app/menu-bar-main.component.js
+++ dev/frontend/app/menu-bar-main.component.js
@@ -11,9 +11,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var MenuBarMainComponent = (function () {
     function MenuBarMainComponent() {
+        this.links = [
+            { path: "/", display: "Home" },
+            { path: "/list", display: "List" },
+            { path: "/detective", display: "Detective" }
+        ];
     }
+    MenuBarMainComponent.prototype.setActiveLink = function (index) {
+        var children = this.navbar.nativeElement.children;
+        for (var i = 0; i < children.length; i++) {
+            var item = children[i];
+            item.classList.remove("active");
+        }
+        this.navbar.nativeElement.children[index].classList.add("active");
+    };
     return MenuBarMainComponent;
 }());
+__decorate([
+    core_1.ViewChild("nav"),
+    __metadata("design:type", Object)
+], MenuBarMainComponent.prototype, "navbar", void 0);
 MenuBarMainComponent = __decorate([
     core_1.Component({
         selector: "menu-bar-main",
--- dev/frontend/app/menu-bar-main.component.ts
+++ dev/frontend/app/menu-bar-main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
   selector: "menu-bar-main",
@@ -8,4 +8,21 @@ import { Component } from "@angular/core";
   ]
 })
 
-export class MenuBarMainComponent {}
+export class MenuBarMainComponent {
+  @ViewChild("nav") navbar: any;
+
+  links = [
+    { path: "/", display: "Home" },
+    { path: "/list", display: "List" },
+    { path: "/detective", display: "Detective" }
+  ];
+
+  setActiveLink(index: number) {
+    let children: HTMLCollection = this.navbar.nativeElement.children;
+    for (let i = 0; i < children.length; i++) {
+      let item: Element = children[i];
+      item.classList.remove("active");
+    }
+    this.navbar.nativeElement.children[index].classList.add("active");
+  }
+}
