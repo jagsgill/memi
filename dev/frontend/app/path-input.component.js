2cb9df8ddc22a690b03b337f142a9b582a493ec0
Jagdeep Gill <jgill.tftf@gmail.com>
2016-12-12 22:08:25 -0800

    completed handling of user clicks on autocomplete entries, switched ChangeDetectorRef to NgZone for forcing re-rendering of PathInput component view

--- dev/frontend/app/path-input.component.js
+++ dev/frontend/app/path-input.component.js
@@ -15,14 +15,13 @@ var disk_usage_for_path_service_1 = require("./disk-usage-for-path.service");
 var list_contents_for_path_service_1 = require("./list-contents-for-path.service");
 var STATUS = require("../../util/errorcodes.js").STATUS;
 var PathInputComponent = (function () {
-    function PathInputComponent(diskQueryService, listDirService, changeDetectorRef) {
+    function PathInputComponent(diskQueryService, listDirService, zone) {
         var _this = this;
         this.diskQueryService = diskQueryService;
         this.listDirService = listDirService;
-        this.changeDetectorRef = changeDetectorRef;
+        this.zone = zone;
         this.iconToParentDir = require("./icons/ic_subdirectory_arrow_right_black_24px.svg");
         this.path = ""; // user path in input box
-        this.dirname = ""; // directory path for autocomplete entries
         this.autocompleteActive = false;
         this.streamListDirResults = listDirService.getResultStream()
             .map(function (result) { return _this.listDirQueryHandler(result); });
@@ -38,25 +37,29 @@ var PathInputComponent = (function () {
         this.streamInputKeyPresses = rxjs_1.Observable.fromEvent(this.pathInputBox.nativeElement, "keydown", function (event) { return event; });
         var slowInputStream = this.streamInputKeyPresses.debounceTime(100);
         this.streamUpdateAutocompleteEntries = slowInputStream
+            .filter(function (event) { return event.key !== "Enter"; })
             .do(function (e) {
-            _this.autocompleteEntries.setFilteredEntries(_this.path);
-            _this.changeDetectorRef.detectChanges();
+            _this.zone.run(function () {
+                _this.autocompleteActive = true;
+                _this.autocompleteEntries.setFilteredEntries(_this.path);
+            });
         })
             .subscribe();
         this.streamEnter = this.streamInputKeyPresses
             .filter(function (event) { return event.key === "Enter"; })
             .do(function (event) {
-            var ae = _this.autocompleteEntries, path;
-            event.preventDefault();
-            event.target.blur(); // hide autocomplete list
-            console.log(ae);
-            if (ae.selected === null) {
-                path = _this.path;
-            }
-            else {
-                path = ae.filteredEntries[ae.selected];
-            }
-            _this.sendDiskUsageQuery(paths.normalize(path));
+            _this.zone.run(function () {
+                var ae = _this.autocompleteEntries, path;
+                event.preventDefault();
+                _this.autocompleteActive = false; // hide autocomplete list
+                if (ae.selected === null) {
+                    path = _this.path;
+                }
+                else {
+                    path = ae.filteredEntries[ae.selected];
+                }
+                _this.sendDiskUsageQuery(paths.normalize(path));
+            });
         }).subscribe();
         this.streamBackspace = this.streamInputKeyPresses
             .filter(function (e) { return e.key === "Backspace"; })
@@ -136,19 +139,22 @@ var PathInputComponent = (function () {
         this.diskQueryService.diskUsage(path);
     };
     PathInputComponent.prototype.diskQueryFinishedHandler = function (result) {
-        this.cwd = result.summary.cwd;
-        this.path = result.summary.cwd;
-        this.changeDetectorRef.detectChanges();
+        var _this = this;
+        this.zone.run(function () {
+            _this.cwd = result.summary.cwd;
+            _this.path = result.summary.cwd;
+        });
     };
     PathInputComponent.prototype.listDirQuery = function (path) {
         this.listDirService.listDirContents(paths.normalize(path));
     };
     PathInputComponent.prototype.listDirQueryHandler = function (result) {
+        var _this = this;
         // save an instance member for template re-rendering
-        var ae = new AutocompleteEntries(result.entries, result.dir);
-        this.autocompleteEntries = ae;
-        this.changeDetectorRef.detectChanges(); // force re-rendering of autocomplete list
-        return ae;
+        this.zone.run(function () {
+            var ae = new AutocompleteEntries(result.entries, result.dir);
+            _this.autocompleteEntries = ae;
+        });
     };
     PathInputComponent.prototype.getDirName = function (path) {
         // return the longest directory path found in the input string
@@ -156,12 +162,17 @@ var PathInputComponent = (function () {
         // whereas paths.dirname("/Users/Applications/") => /Users/
         return path[path.length - 1] === paths.sep ? path : paths.dirname(path);
     };
+    PathInputComponent.prototype.liClickHandler = function (event) {
+        var _this = this;
+        this.zone.run(function () {
+            event.preventDefault();
+            _this.autocompleteActive = false;
+            var path = event.target.text; // entry user clicked on
+            _this.sendDiskUsageQuery(paths.normalize(path));
+        });
+    };
     return PathInputComponent;
 }());
-__decorate([
-    core_1.ViewChild("pathSubmitter"),
-    __metadata("design:type", Object)
-], PathInputComponent.prototype, "pathSubmitter", void 0);
 __decorate([
     core_1.ViewChild("pathInputBox"),
     __metadata("design:type", Object)
@@ -176,7 +187,7 @@ PathInputComponent = __decorate([
     }),
     __metadata("design:paramtypes", [disk_usage_for_path_service_1.DiskUsageService,
         list_contents_for_path_service_1.ListDirService,
-        core_1.ChangeDetectorRef])
+        core_1.NgZone])
 ], PathInputComponent);
 exports.PathInputComponent = PathInputComponent;
 var AutocompleteEntries = (function () {
--- dev/frontend/app/path-input.component.ts
+++ dev/frontend/app/path-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef, ViewChild } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef, NgZone, ViewChild } from "@angular/core";
 import { Observable, Subscription } from "rxjs";
 import * as paths from "path";
 
@@ -17,10 +17,8 @@ const STATUS = require("../../util/errorcodes.js").STATUS;
 
 export class PathInputComponent implements OnInit, AfterViewInit {
     iconToParentDir = require("./icons/ic_subdirectory_arrow_right_black_24px.svg");
-    @ViewChild("pathSubmitter") pathSubmitter: any;
     @ViewChild("pathInputBox") pathInputBox: any;
     path = ""; // user path in input box
-    dirname = ""; // directory path for autocomplete entries
     cwd: string; // current working directory for analysis in output view TODO remove, belongs in output view routing
     autocompleteEntries: AutocompleteEntries;
     autocompleteActive = false;
@@ -36,7 +34,7 @@ export class PathInputComponent implements OnInit, AfterViewInit {
     constructor(
         private diskQueryService: DiskUsageService,
         private listDirService: ListDirService,
-        private changeDetectorRef: ChangeDetectorRef
+        private zone: NgZone
     ) {
         this.streamListDirResults = listDirService.getResultStream()
             .map((result: any) => { return this.listDirQueryHandler(result) });
@@ -56,26 +54,30 @@ export class PathInputComponent implements OnInit, AfterViewInit {
         let slowInputStream = this.streamInputKeyPresses.debounceTime(100);
 
         this.streamUpdateAutocompleteEntries = slowInputStream
+            .filter(event => event.key !== "Enter")
             .do(e => {
-                this.autocompleteEntries.setFilteredEntries(this.path);
-                this.changeDetectorRef.detectChanges();
+                this.zone.run(() => {
+                    this.autocompleteActive = true;
+                    this.autocompleteEntries.setFilteredEntries(this.path);
+                })
             })
             .subscribe();
 
         this.streamEnter = this.streamInputKeyPresses
             .filter(event => event.key === "Enter")
             .do(event => {
-                let ae = this.autocompleteEntries,
-                    path: string;
-                event.preventDefault();
-                event.target.blur(); // hide autocomplete list
-                console.log(ae);
-                if (ae.selected === null) { // use path in input box
-                    path = this.path;
-                } else { // use selected path in autocomplete list
-                    path = ae.filteredEntries[ae.selected];
-                }
-                this.sendDiskUsageQuery(paths.normalize(path));
+                this.zone.run(() => {
+                    let ae = this.autocompleteEntries,
+                        path: string;
+                    event.preventDefault();
+                    this.autocompleteActive = false; // hide autocomplete list
+                    if (ae.selected === null) { // use path in input box
+                        path = this.path;
+                    } else { // use selected path in autocomplete list
+                        path = ae.filteredEntries[ae.selected];
+                    }
+                    this.sendDiskUsageQuery(paths.normalize(path));
+                })
             }).subscribe();
 
         this.streamBackspace = this.streamInputKeyPresses
@@ -155,21 +157,22 @@ export class PathInputComponent implements OnInit, AfterViewInit {
     }
 
     diskQueryFinishedHandler(result: any): void {
-        this.cwd = result.summary.cwd;
-        this.path = result.summary.cwd;
-        this.changeDetectorRef.detectChanges();
+        this.zone.run(() => {
+            this.cwd = result.summary.cwd;
+            this.path = result.summary.cwd;
+        });
     }
 
     listDirQuery(path: string): void {
         this.listDirService.listDirContents(paths.normalize(path));
     }
 
-    listDirQueryHandler(result: any): AutocompleteEntries {
+    listDirQueryHandler(result: any): void {
         // save an instance member for template re-rendering
-        let ae = new AutocompleteEntries(result.entries, result.dir);
-        this.autocompleteEntries = ae;
-        this.changeDetectorRef.detectChanges(); // force re-rendering of autocomplete list
-        return ae;
+        this.zone.run(() => {
+            let ae = new AutocompleteEntries(result.entries, result.dir);
+            this.autocompleteEntries = ae;
+        });
     }
 
     getDirName(path: string): string {
@@ -177,6 +180,15 @@ export class PathInputComponent implements OnInit, AfterViewInit {
         // /Users/Applications/ => /Users/Applications/
         // whereas paths.dirname("/Users/Applications/") => /Users/
         return path[path.length - 1] === paths.sep ? path : paths.dirname(path);
+    }
+
+    liClickHandler(event: any): void {
+        this.zone.run(() => {
+            event.preventDefault();
+            this.autocompleteActive = false;
+            let path = event.target.text; // entry user clicked on
+            this.sendDiskUsageQuery(paths.normalize(path));
+        });
     }
 }
 
